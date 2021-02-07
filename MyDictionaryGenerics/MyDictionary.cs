fe430cb1a9ba16f828034b0b1d2cf502fb7be39b
Isilboyuk <e201593@metu.edu.tr>
2021-02-07 23:53:48 +0300

    Initial commit

---
+++ MyDictionaryGenerics/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryGenerics
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] key;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            key = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey keyVal, TValue value)
+        {
+            //To keep initial values of key and values tempKeys&tempValues created.
+            TKey[] tempKeys = key;
+            TValue[] tempValues = values;
+
+            key = new TKey[key.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            //Assign first values
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                key[i] = tempKeys[i];
+                values[i] = tempValues[i];
+
+            }
+            //Add new values
+            key[tempKeys.Length] = keyVal;
+            values[tempValues.Length] = value;
+        }
+
+    }
+}
---
+++ MyDictionaryGenerics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionaryGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string,string> myDictionary = new MyDictionary<string,string>();
+            myDictionary.Add("Product","Ürün");
+            myDictionary.Add("Customer", "Müşteri");
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
